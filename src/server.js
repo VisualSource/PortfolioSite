a109eeaa26a8b00270863f5df78acd7416434b05
BoomIsHere <43074703+BoomIsHere@users.noreply.github.com>
2019-05-29 10:09:42 -0400

    Server 1.2.2

--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
 const port = process.env.PORT;
+const users = {
+  user : [{ name: Collin, cId: "435k435", icon: "fav.png"}]
+}
 app.use(bodyparser.json());
 app.use(express.static("public"));
-
-app.use("*", (req, res)=> {
+app.get('/profile/:id',(req,res)=>{
+    res.json();
+});
+app.get("*", (req, res)=> {
   res.end(); 
 });
 app.listen(port, () => {
