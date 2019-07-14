37e6aa817d78c34d23a2c987f642569d3f5a9a2f
BoomIsHere <43074703+BoomIsHere@users.noreply.github.com>
2019-07-13 23:21:27 -0400

    Update 0.1.2

---
+++ precache-manifest.f7ace258d055850d5b95e47fcfc9cf71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a46ce29b3a8658b844d6d1176e64ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67f64b0676e1ed9ffbcf",
+    "url": "/static/css/main.099860e4.chunk.css"
+  },
+  {
+    "revision": "1392e95223718093d87f",
+    "url": "/static/js/2.2612989e.chunk.js"
+  },
+  {
+    "revision": "67f64b0676e1ed9ffbcf",
+    "url": "/static/js/main.c0c5e8a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "575e6ae802c0e41f7cfa165b0ce7dcaf",
+    "url": "/static/media/AdequateLight.575e6ae8.ttf"
+  },
+  {
+    "revision": "4b2be8756bbb2c80ff777b989df224d6",
+    "url": "/static/media/cube.4b2be875.svg"
+  }
+]);
