8b4130aacb5c6b04454c813ce6acf65d915039aa
Collin Blosser <BoomIsHere_Network@outlook.com>
2019-07-13 23:29:48 -0400

    Patch-0.1.3

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.522536f9799672263e2909d9b323f3c4.js"
+  "/precache-manifest.f7ace258d055850d5b95e47fcfc9cf71.js"
 );
 
 self.addEventListener('message', (event) => {
