7edfae324f4b69c3825cc7a09023d5abc6b3c844
BoomIsHere <43074703+BoomIsHere@users.noreply.github.com>
2019-09-23 11:37:58 -0400

    files

--- server.js
+++ server.js
@@ -1,69 +1,88 @@
 const PORT = process.env.PORT || 8000;
-const path = require('path');
-const wsHandler = require('./src/controllers/wsHandler');
-const fastify = require('fastify')({ logger: false });
+const DATABASE_URL = process.env.DATABASE_URL || "http://localhost:5001";
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "root";
+const path = require("path");
+const wsHandler = require("./src/controllers/wsHandler");
+const fastify = require("fastify")({ logger: false });
 
-function getThroneRoom(faction = ""){
-   console.log(faction)
-    return {data:[]};
+function getThroneRoom(faction = "") {
+  console.log(faction);
+  return { data: [] };
 }
 /**
- * 2xx Succes 
- *  
- *  200 Ok 
+ * 2xx Succes
+ *
+ *  200 Ok
  *  202 Accepted
  *  204 No Content
- * 
- * 
+ *
+ *
  * 4xx Client Errors
- * 
+ *
  *  400 Bad Request
  *  401 unauthorized
- *  403 Forbidden 
- *  404 Not Found 
- *  406 Not Acceptable 
+ *  403 Forbidden
+ *  404 Not Found
+ *  406 Not Acceptable
  *  409 Conflict
  */
-fastify.register(require('fastify-websocket'));
-fastify.register(require('fastify-static'), {
-    root: path.join(__dirname, 'public')
-  });
+fastify.register(require("fastify-websocket"));
 
-
-fastify.get('/polytopia', { websocket: true }, wsHandler);
-
-
-fastify.post('/user/:id', (request,reply)=>{
-  reply.status(409).send({error:"Conflict"})
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "public")
 });
-fastify.post('/throneroom/:faction', async (req,rep)=>{
-  let data = await getThroneRoom(req.params.faction)
+const bookshelf = require("fastify-bookshelfjs");
+fastify.register(
+  bookshelf,
+  {
+    client: "pg",
+    connection: {
+      host: DATABASE_URL,
+      user: "host",
+      password: DATABASE_PASSWORD,
+      database: "user"
+    }
+  },
+  console.error
+);
+
+fastify.use(require("sanitize").middleware);
+
+fastify.get("/polytopia", { websocket: true }, wsHandler);
+
+fastify.post("/user/:id", (request, reply) => {
+  reply.status(409).send({ error: "Conflict" });
+});
+fastify.post("/throneroom/:faction", async (req, rep) => {
+  let data = await getThroneRoom(req.params.faction);
   return data;
 });
-fastify.get('/', async (request, reply) => {
-  reply.sendFile('index.html')
+fastify.get("/", async (request, reply) => {
+  reply.sendFile("index.html");
 });
 
-
-fastify.setNotFoundHandler({
-  preValidation: (req, reply, done) => {
-    done()
+fastify.setNotFoundHandler(
+  {
+    preValidation: (req, reply, done) => {
+      done();
+    },
+    preHandler: (req, reply, done) => {
+      done();
+    }
   },
-  preHandler: (req, reply, done) => {
-    done()
+  (request, reply) => {
+    reply.sendFile("404.html");
   }
-}, (request, reply)=> {
-    reply.sendFile('404.html')
-})
+);
 
 const start = async () => {
   try {
-    await fastify.listen(PORT)
+    await fastify.listen(PORT);
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
-    console.log(`server listening on ${fastify.server.address().port}`)
+    console.log(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-}
-start()
+};
+start();
