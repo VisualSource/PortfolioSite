850bd57eeff2862bee7c4fc2bb612bc9c9f5fd15
VisualSource <boomishere_network@outlook.com>
2021-01-03 19:44:22 -0500

    changes

--- server.js
+++ server.js
@@ -2,81 +2,37 @@
 const PORT = process.env.PORT || 8000
 const DEV = process.env.DEV || true;
 
-//node modules
-const path = require("path");
-//const fs = require("fs");
-const helment = require('fastify-helmet');
-const jwt = require('jsonwebtoken');
-const fastify = require("fastify")({ http2: false, logger: false}); // https: { key: fs.readFileSync('https/key.pem'), cert: fs.readFileSync('https/server.crt') }
+const mcpeping = require('mcpe-ping');
+const express = require('express');
+const cors = require('cors')
+const app = express();
 
-// custom files
-const controllers = require('./controllers/controllers');
-const wsHandler = require("./controllers/wsHandler");
+app.use(cors({
+    origin: ["http://localhost:3000","https://visualsource.000webhostapp.com/"],
+}));
+app.use(express.json());
 
-
-fastify.register(require('./plugins/ws'));
-fastify.register(require('fastify-cookie'));
-fastify.register(helment,{dnsPrefetchControl: false,});
-const origins = DEV ? ["http://locahost:3000","http://127.0.0.1:5500","https://127.0.0.1:8000","http://127.0.0.1:8000","https://visualsource.000webhostapp.com","https://visualsource.herokuapp.com/"] : ["https://visualsource.000webhostapp.com","https://visualsource.herokuapp.com/"];
-fastify.register(require('fastify-cors'), {
- origin: origins
+app.get('/', function (req, res) {
+    mcpeping("135.148.9.125", 19132,(err,data)=>{
+      if (err) {
+        res.status(200).json({type:"error",  data:err});
+     } else {
+         res.status(200).json( {
+             type:"info", 
+             code: 200,
+             data: {
+                 players: [],
+                 max_players: data.maxPlayers,
+                 current_players: data.currentPlayers
+             }, 
+             msg:"returning data"
+         });
+     }
+    },3000,false);
 });
-fastify.register(require("fastify-static"), {root: path.join(__dirname, "public")});
-
-//websocket
-fastify.ready(err => {
-  if (err) throw err
-  console.info("Websocket online")
-  fastify.ws.on('connection',socket=>{wsHandler(socket,fastify.ws)})
-});
-
-// routes
-fastify.decorate('bearerAuth', async function (request, reply) {
-    const requestKey = await request.headers.authorization.replace("Bearer ","");
-    jwt.verify(requestKey, '48133e08666f4a2a9ecdd1692f429fe3', function(err, decoded) {
-      if(err) return new Error(err.name);
-      if(decoded.host !== "auth0") return new Error("Invaild Host");
-      return "OK";
-    });
-  }).
-register(require('fastify-auth')).after(() => {
-    fastify.route({
-      method: 'POST',
-      url: '/adduser/:id',
-      preHandler: fastify.auth([fastify.bearerAuth]),
-      handler: controllers.routeAddUser
-    })
-  });
-
-fastify.route(controllers.routeThrownRoom);
-fastify.route(controllers.routeCreate);
-fastify.get("/", async (request, reply) => { reply.sendFile("index.html"); });
+ 
+app.listen(PORT,"0.0.0.0");
 
 
 
-// handlers
-fastify.setNotFoundHandler({
-       preValidation: (req, reply, done) => { done(); },
-       preHandler: (req, reply, done) => { done(); }
-  },
-  (request, reply) => { reply.sendFile("404.html");}
-);
-
-fastify.setErrorHandler( (err, req, reply) => {
-  if (err.statusCode === 401) { reply.code(401).send({ payload: 'unauthorized' }) };
-  console.error(err)
-  reply.send({error: err.statusCode, errorHandler: true})
-});
-
-// start
-const start = async () => {
-  try {
-    await fastify.listen(PORT,"0.0.0.0");
-    console.log(PORT)
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-};
-start();
-
+//35.209.27.81
