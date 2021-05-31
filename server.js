8ae7e790d9d4a62d9b02d73d1670aef4ee6d8949
VisualSource <boomishere_network@outlook.com>
2021-05-31 18:49:18 -0400

    cors

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 
-app.use(cors({
-    origin: ["http://localhost:8000","https://visualsource.000webhostapp.com/"],
-}));
+app.use(cors());
 app.use(express.json());
 
 app.get("/favicon.ico",(req,res)=>{
