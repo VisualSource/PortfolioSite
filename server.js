97b5badd44e7e52a178203a03fde5839917a264d
VisualSource <boomishere_network@outlook.com>
2021-05-31 18:47:10 -0400

    ssl

--- server.js
+++ server.js
@@ -2,15 +2,24 @@
 const PORT = process.env.PORT || 8000
 const DEV = process.env.DEV || true;
 
+const path = require("path");
+
 const mcpeping = require('mcpe-ping');
 const express = require('express');
 const cors = require('cors')
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:3000","https://visualsource.000webhostapp.com/"],
+    origin: ["http://localhost:8000","https://visualsource.000webhostapp.com/"],
 }));
 app.use(express.json());
+
+app.get("/favicon.ico",(req,res)=>{
+    res.sendFile(path.join(__dirname,"favicon.ico"));
+});
+app.get("/.well-known/acme-challenge/oAw_3Sz9Otg8Esqw1-EB7FK1HHm4eLLUnsw53aag7Ik",(req,res)=>{
+    res.sendFile(path.join(__dirname,"vaild.txt"));
+});
 
 app.get('/', function (req, res) {
     mcpeping("35.209.27.81", 19132,(err,data)=>{
@@ -30,6 +39,7 @@ app.get('/', function (req, res) {
      }
     },3000,false);
 });
+
  
 app.listen(PORT,"0.0.0.0");
 
