9dc698c57ad293d1e2440f39cb04683b08445e3a
VisualSource <boomishere_network@outlook.com>
2021-06-26 17:24:10 -0400

    polytopia 0.5.0 demo

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const cors = require('cors')
 const app = express();
 const server = http.createServer(app);
 
+
+app.use(express.static('public'));
+
 const { Server } = require("socket.io");
 
 const io = new Server(server);
@@ -25,18 +28,8 @@ app.use(express.json());
 app.get("/socket.io/socket.io.js",(req,res)=>{
     res.sendFile(path.join(__dirname,"node_modules/socket.io/client-dist/socket.io.min.js"));
 });
-app.get("/favicon.ico",(req,res)=>{
-    res.sendFile(path.join(__dirname,"public","favicon.ico"));
-});
-app.get("/.well-known/acme-challenge/EA0YdZf2zqaLafGJJQIsqMlc51yhMf70nTxf8Icj3nE",(req,res)=>{
-    res.sendFile(path.join(__dirname,"public","vaild.txt"));
-});
-app.get("/socket-test",(req,res)=>{
-    res.sendFile(path.join(__dirname,"public","socket_test.html"));
-});
-
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname,"public","index.html"));
+    res.sendFile(path.join(__dirname,"public","tests.html"));
 });
 
 server.listen(PORT,"0.0.0.0");
