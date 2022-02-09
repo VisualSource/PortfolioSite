b5a626c4ffb792b1cc84e4d37ea31e6a498cee0c
VisualSource <boomishere_network@outlook.com>
2022-02-08 21:50:14 -0500

    models fixes

--- models/game2048.js
+++ models/game2048.js
@@ -3,16 +3,8 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Game2048 extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
+  class Game2048 extends Model {}
+  
   Game2048.init({
     user: {
       allowNull: false,
--- models/mcs_resources.js
+++ models/mcs_resources.js
@@ -9,16 +9,8 @@ const {
  * @returns 
  */
 module.exports = (sequelize, DataTypes) => {
-  class MCSResources extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
+  class MCSResources extends Model {}
+  
   MCSResources.init({
     id: {
       type: DataTypes.INTEGER,
